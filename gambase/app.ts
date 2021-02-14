419521ff05614932dcdd9a8acfb89d2780353603
Dan Bock <daniel.bock@sscinc.com>
2021-02-14 15:06:05 -0500

    Misc fixes

--- gambase/app.ts
+++ gambase/app.ts
@@ -14,13 +14,13 @@ connection.connect(err => console.log('connected: ' + connection.threadId + ' er
 console.log(new Date().toString(), 'Server started up')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.writeHead(200, { 'Content-Type': 'application/json' })
 
-  console.log(new Date().toString(), `New request: ${JSON.stringify(req.headers)}`)
+  console.log(new Date().toString(), `New request: ${req.url}`)
 
   connection.query('select mydata from deleteme', (e, r, f) => {
     console.log('Database response', { e, r, f })
-    res.end(JSON.stringify({ e, r, f }))
+    res.end(JSON.stringify(r))
   })
 })
 
