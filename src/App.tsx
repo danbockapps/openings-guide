721604daeef2045294c76237af18bdd9af0c380c
Dan Bock <daniel.bock@sscinc.com>
2021-02-18 17:45:58 -0500

    Move state to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import axios from 'axios'
+import * as ChessJS from 'chess.js'
+import { FC, useState } from 'react'
 import './App.scss'
 import Board from './components/Board'
 
-function App() {
+// https://stackoverflow.com/a/65243150/400765
+const Chess = typeof ChessJS === 'function' ? ChessJS : ChessJS.Chess
+
+const App: FC = () => {
+  const [history, setHistory] = useState<string[]>([])
+
+  const chess = new Chess()
+  history.forEach(move => chess.move(move))
+
   return (
     <div className="App">
-      <Board />
+      <Board
+        position={chess.fen()}
+        onDrop={({ sourceSquare, targetSquare }) => {
+          chess.move({ from: sourceSquare, to: targetSquare })
+          console.log(chess.fen())
+          setHistory(chess.history())
+
+          axios
+            .get('https://danbock.net/gambase', { params: { fen: chess.fen() } })
+            .then(response => console.log(response.data))
+        }}
+      />
       <div
         style={{
           background: 'yellow',
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,34 +1,22 @@
-import axios from 'axios'
-import * as ChessJS from 'chess.js'
+import { Square } from 'chess.js'
 import Chessboard from 'chessboardjsx'
-import { FC, useState } from 'react'
+import { FC } from 'react'
 
-// https://stackoverflow.com/a/65243150/400765
-const Chess = typeof ChessJS === 'function' ? ChessJS : ChessJS.Chess
+interface Props {
+  position: string
+  onDrop: (obj: { sourceSquare: Square; targetSquare: Square }) => void
+}
 
-const Board: FC = () => {
-  const [history, setHistory] = useState<string[]>([])
-
-  const chess = new Chess()
-  history.forEach(move => chess.move(move))
-
+const Board: FC<Props> = props => {
   return (
     <Chessboard
-      position={chess.fen()}
+      position={props.position}
       calcWidth={({ screenWidth, screenHeight }) => {
         if (screenHeight > screenWidth) /* vertical orientation */ return screenWidth
         else if (screenWidth - 300 < screenHeight) /* horiz but squarish */ return screenWidth - 300
         else return screenHeight
       }}
-      onDrop={({ sourceSquare, targetSquare }) => {
-        chess.move({ from: sourceSquare, to: targetSquare })
-        console.log(chess.fen())
-        setHistory(chess.history())
-
-        axios
-          .get('https://danbock.net/gambase', { params: { fen: chess.fen() } })
-          .then(response => console.log(response.data))
-      }}
+      onDrop={props.onDrop}
     />
   )
 }
