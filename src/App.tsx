5e21be2e882c12428b530e33e57b61a412887999
Dan Bock <daniel.bock@sscinc.com>
2021-02-18 05:39:05 -0500

    CSS work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css'
+import './App.scss'
 
 function App() {
-  return <div className="App">Hello world</div>
+  return (
+    <div className="App">
+      <div
+        style={{
+          background: 'blue',
+        }}
+      />
+      <div
+        style={{
+          background: 'yellow',
+        }}
+      />
+    </div>
+  )
 }
 
 export default App
