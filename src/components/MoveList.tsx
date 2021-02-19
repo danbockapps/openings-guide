bd9978f51d5375dfb41c75ec3bca35c79dc03b18
Dan Bock <daniel.bock@sscinc.com>
2021-02-18 23:29:08 -0500

    Styling for move list

--- src/components/MoveList.tsx
+++ src/components/MoveList.tsx
@@ -1,16 +1,30 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { NextMove } from '../types'
 
 interface Props {
   nextMoves?: NextMove[]
 }
 
-const MoveList: FC<Props> = props => (
-  <div className="move-list">
-    {props.nextMoves?.map((n, i) => (
-      <div key={i}>{n.move}</div>
-    ))}
-  </div>
-)
+const MoveList: FC<Props> = props => {
+  const [hover, setHover] = useState<number>()
+
+  return (
+    <table className="move-list">
+      <tbody>
+        {props.nextMoves?.map((n, i) => (
+          <tr
+            key={i}
+            className={hover === i ? 'hover' : undefined}
+            onMouseOver={() => setHover(i)}
+            onMouseOut={() => setHover(undefined)}
+          >
+            <td>{n.move}</td>
+            <td>{n.count}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default MoveList
