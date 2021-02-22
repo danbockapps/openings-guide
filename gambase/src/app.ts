0ce6e6d5de86286e5d943e73741b79bab63fd1dd
Dan Bock <daniel.bock@sscinc.com>
2021-02-22 18:30:11 -0500

    Refactor

--- gambase/src/app.ts
+++ gambase/src/app.ts
@@ -1,4 +1,4 @@
-import http from 'http'
+import http, { ServerResponse } from 'http'
 import { parse } from 'querystring'
 import { getNextMoves, getTotalGames } from './functions'
 
@@ -11,22 +11,21 @@ const server = http.createServer((req, res) => {
   const { fen } = parse(req.url?.split('?')[1] || '')
   const totalGames = req.url?.includes('totalGames')
 
-  if (totalGames) {
-    res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-    getTotalGames().then(r => {
-      res.end(JSON.stringify(r[0]?.count))
-      console.timeEnd(req.url)
-    })
-  } else if (typeof fen !== 'string') {
+  if (totalGames) getTotalGames().then(r => success(res, r[0]?.count, req.url))
+  else if (typeof fen === 'string')
+    Promise.all([getNextMoves(fen, 1), getNextMoves(fen, 5)]).then(r =>
+      success(res, { next1: r[0], next5: r[1] }, req.url),
+    )
+  else {
     res.writeHead(400)
     res.end()
-  } else {
-    res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-    Promise.all([getNextMoves(fen, 1), getNextMoves(fen, 5)]).then(r => {
-      res.end(JSON.stringify({ next1: r[0], next5: r[1] }))
-      console.timeEnd(req.url)
-    })
   }
 })
+
+const success = (res: ServerResponse, value: any, url?: string) => {
+  res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
+  res.end(JSON.stringify(value))
+  console.timeEnd(url)
+}
 
 server.listen()
